8c786509949953d3c8722d1eceddd37b7d472ff9
Martin Wood <martin@martinwebdev.co.uk>
2019-04-12 09:40:01 +1000

    Setup basic example template ready for development

--- index.dev.js
+++ index.dev.js
@@ -1,5 +1,6 @@
-import './examples/start-simple';
-import './examples/add-routing';
-import './examples/add-animation';
-import './examples/add-progress-bar';
-import './examples/skip-a-step';
+// import './examples/start-simple';
+// import './examples/add-routing';
+// import './examples/add-animation';
+// import './examples/add-progress-bar';
+// import './examples/skip-a-step';
+import './examples/conditional-steps';
