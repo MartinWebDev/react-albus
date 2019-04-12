bb140ccda2b76e95ebfbf3bfce46ba74d3e51408
Martin Wood <martin@martinwebdev.co.uk>
2019-04-12 18:05:51 +1000

    Simplify basic template

--- examples/conditional-steps/index.js
+++ examples/conditional-steps/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { Wizard, Steps, Step } from '../../src';
-import { Line } from 'rc-progress';
 import Navigation from './Navigation';
 import './exampleAnimation.css';
 
@@ -16,56 +13,31 @@ const data = [
 ];
 
 const ConditionalSteps = () => (
-  <BrowserRouter>
-    <div className="row pad-t">
-      <div className="col-xs-6 col-xs-offset-3">
-        <Route
-          render={({ history }) => (
-            <Wizard
-              history={history}
-              render={({ step, steps }) => (
-                <div>
-                  <Line
-                    percent={(steps.indexOf(step) + 1) / steps.length * 100}
-                    className="pad-b"
-                  />
-                  <TransitionGroup>
-                    <CSSTransition
-                      key={step.id}
-                      classNames="example"
-                      timeout={{ enter: 500, exit: 500 }}
-                    >
-                      <div className="example-steps fluid">
-                        <Steps key={step.id} step={step}>
-                          <Step id="gandalf">
-                            <h1 className="text-align-center">Gandalf</h1>
-                          </Step>
-                          <Step id="dumbledore">
-                            <h1 className="text-align-center">Dumbledore</h1>
-                          </Step>
-                          {
-                            data.map((el) => (
-                              <Step id={`step-${el.id}`} key={el.id} enabled={el.show}>
-                                <h1>{el.text}</h1>
-                              </Step>
-                            ))
-                          }
-                          <Step id="ice-king">
-                            <h1 className="text-align-center">Ice King</h1>
-                          </Step>
-                        </Steps>
-                      </div>
-                    </CSSTransition>
-                  </TransitionGroup>
-                  <Navigation />
-                </div>
-              )}
-            />
-          )}
-        />
-      </div>
+  <div className="row pad-t">
+    <div className="col-xs-6 col-xs-offset-3">
+      <Wizard>
+        <Steps>
+          <Step id="gandalf">
+            <h1 className="text-align-center">Gandalf</h1>
+          </Step>
+          <Step id="dumbledore">
+            <h1 className="text-align-center">Dumbledore</h1>
+          </Step>
+          {
+            data.map((el) => (
+              <Step id={`step-${el.id}`} key={el.id} enabled={el.show}>
+                <h1>{el.text}</h1>
+              </Step>
+            ))
+          }
+          <Step id="ice king">
+            <h1 className="text-align-center">Ice King</h1>
+          </Step>
+        </Steps>
+        <Navigation />
+      </Wizard>
     </div>
-  </BrowserRouter>
+  </div>
 );
 
 render(<ConditionalSteps />, document.getElementById('conditional-steps'));
